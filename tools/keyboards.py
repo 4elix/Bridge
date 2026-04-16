7141cedf616dcdd021eca8a682f936e4529237e7
unknown <sorokseme@gmail.com>
2026-04-16 12:24:09 +0500

    Creating a function for creating FAQs

--- tools/keyboards.py
+++ tools/keyboards.py
@@ -12,11 +12,20 @@ kb_finish_register = InlineKeyboardMarkup(
 
 def kb_start_menu(role):
     func = ['Часто задаваемые вопрос', 'Написать менеджеру']
-    if role == 'client':
-        btn = [[KeyboardButton(text=b)] for b in func]
-    elif role == 'admin':
-        func.append('Работа с вопросами')
-        btn = [[KeyboardButton(text=b)] for b in func]
+    if role == 'admin':
+        func.append('Создать вопрос')
 
+    btn = [[KeyboardButton(text=b)] for b in func]
     markup = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=btn)
     return markup
+
+
+kb_create_faq = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Создать новый вопрос', callback_data='create_faq')],
+    [InlineKeyboardButton(text='Назад в меню', callback_data='back_to_menu')]
+])
+
+kb_save_faq = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Сохранить', callback_data='save_faq')],
+    [InlineKeyboardButton(text='Не сохранять', callback_data='dont_save_faq')]
+])
--- tools/states.py
+++ tools/states.py
@@ -3,3 +3,8 @@ from aiogram.fsm.state import State, StatesGroup
 
 class RegisterUser(StatesGroup):
     first_name = State()
+
+
+class CreateFaq(StatesGroup):
+    question = State()
+    answer = State()
